2a79cfb5431bad9e3b471c19ea7c2b99a845fe3a
Jan Vrany <jan.vrany@fit.cvut.cz>
2022-05-13 20:09:52 +0000

    Improve `prepare.sh` to `git pull` changes
    ...into already cloned repos. This eases updating.

--- prepare.sh
+++ prepare.sh
@@ -14,14 +14,20 @@
 
 if [ ! -d openj9-openjdk-jdk11 ]; then
 	git clone --branch "$openjdk_branch" "$openjdk_repo" openj9-openjdk-jdk11        --depth 1
+else
+	git -C openj9-openjdk-jdk11 pull
 fi
 
 if [ ! -d openj9-openjdk-jdk11/omr ]; then
 	git clone --branch "$omr_branch"     "$omr_repo"     openj9-openjdk-jdk11/omr
+else
+	git -C openj9-openjdk-jdk11/omr pull
 fi
 
 if [ ! -d openj9-openjdk-jdk11/openj9 ]; then
 	git clone --branch "$openj9_branch"  "$openj9_repo"  openj9-openjdk-jdk11/openj9
+else
+	git -C openj9-openjdk-jdk11/openj9 pull
 fi
 
 if [ ! -f "apache-freemarker-${freemarker_version}-bin/freemarker.jar" ]; then
